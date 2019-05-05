585fc13b03d82cfc182164b7e292389b139dca72
Roman Bulinski <aboandrus@gmail.com>
2019-05-05 21:13:18 +0200

    Implemented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,23 @@
 public class Main {
 
     public static int gps(int s, double[] x) {
-        // your code
+
+
+        double delta_distance = 0;
+
+        for( int i =1; i<x.length; i++){
+            if( x[i]-x[i-1] > delta_distance ){
+                delta_distance = x[i]-x[i-1];
+            }
+        }
+
+        int speed = (int)((3600 * delta_distance) / s);
+
+        return speed;
+
+
+
     }
+
 
 }
