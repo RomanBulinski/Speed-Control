601e7aad60aa0821035777ea87d678687d29c265
Roman Bulinski <aboandrus@gmail.com>
2019-05-05 21:23:52 +0200

    Added second test

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -4,17 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMain {
 
-
 //    @Test
 //    public void test00() {
 //        System.out.println("Fixed Tests: gps");
 //        double[] x = new double[]{0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61};
 //        testing(GpsSpeed.gps(20, x), 41);
+
 //        x = new double[]{0.0, 0.11, 0.22, 0.33, 0.44, 0.65, 1.08, 1.26, 1.68, 1.89, 2.1, 2.31, 2.52, 3.25};
 //        testing(GpsSpeed.gps(12, x), 219);
+
 //        x = new double[]{0.0, 0.18, 0.36, 0.54, 0.72, 1.05, 1.26, 1.47, 1.92, 2.16, 2.4, 2.64, 2.88, 3.12, 3.36, 3.6, 3.84};
 //    }
-
 
     @Test
     public void test1() {
@@ -22,6 +22,10 @@ public class TestMain {
         assertEquals(41, Main.gps(20, arr));
     }
 
-    
+    @Test
+    public void test2() {
+        double[] arr = new double[]{0.0, 0.11, 0.22, 0.33, 0.44, 0.65, 1.08, 1.26, 1.68, 1.89, 2.1, 2.31, 2.52, 3.25};
+        assertEquals(219, Main.gps(12, arr));
+    }
 
 }
